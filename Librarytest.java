9d6609e73f469d299dedee7d93152c7e1b52ef11
alhmsiSWE <ffahad7120@gmail.com>
2025-11-21 20:18:47 +0300

    Add files via upload
    Here is the initial main method for project 2.

---
+++ Librarytest.java
@@ -0,0 +1,16 @@
+package phase2;
+
+import java.util.Scanner;
+
+public class Librarytest {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		Member fahad = new Member(446100868, "Fahad");
+		Member yazeed = new Member(446101220, "Yazeed");
+		Member saif = new Member(446106919, "Saif");
+
+		System.out.println("Welcome to library");
+	}
+}	
